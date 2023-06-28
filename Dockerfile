4c849fbfcfafec85699d5b83aac87085fa2995ad
cthiounn2 <thiounn.conrad@gmail.com>
2023-06-28 12:25:59 +0000

    add app docker

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# syntax = docker/dockerfile:1.2
+FROM python:3.10
+
+COPY requirements.txt /tmp/
+RUN pip install --requirement /tmp/requirements.txt
+#RUN --mount=type=cache,target=/var/cache/pip pip install --requirement /tmp/requirements.txt
+
+WORKDIR /app
+# Copy all the files of this project inside the container
+COPY . .
+
+RUN --mount=type=cache,target=/var/cache/ curl -SL https://minio.lab.sspcloud.fr/cthiounn2/ckpt_dares_ceren_bert_multi.pth -o ckpt_dares_ceren_bert_multi.pth 
+
+CMD ["streamlit", "run", "streamlit-api.py","--server.port", "3838"]
+
---
+++ streamlit-api.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import torch
+from transformers import BertTokenizer, BertForSequenceClassification
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased")
+model = BertForSequenceClassification.from_pretrained("bert-base-multilingual-cased", num_labels=5)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model=model.to(device)
+model.load_state_dict(torch.load("ckpt_dares_ceren_bert_multi.pth", map_location=device))
+
+st.text("Version : 1.0 ")
+
+titre = st.text_input('Titre')
+commentaire = st.text_area('Commentaire')
+
+if titre and commentaire :
+    text=f'TITRE: {titre}' ' |AND| ' + 'COMMENT: {commentaire}'
+    inputs =tokenizer.encode_plus(
+                text,
+                None,
+                add_special_tokens=True,
+                max_length=128,
+                padding="max_length",
+                truncation=True
+            )
+    
+    input_ids = torch.tensor(inputs["input_ids"]).unsqueeze(1).permute(1,0)
+    attention_mask = torch.tensor(inputs["attention_mask"]).unsqueeze(1).permute(1,0)
+    token_type_ids = torch.tensor(inputs["token_type_ids"]).unsqueeze(1).permute(1,0)
+    
+    input_ids = input_ids.to(device)
+    attention_mask = attention_mask.to(device)
+    token_type_ids = token_type_ids.to(device)
+    
+    
+    outputs = model(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+    
+    loss = outputs.loss
+    logits = outputs.logits
+    
+    _, predicted = torch.max(logits, dim=1)
+
+    for _ in range(int(predicted[0])):
+        st.text('♥')
+
